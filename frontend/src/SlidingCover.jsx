052c6ca5500c5edf3a2055ce840b25013b3cbd58
Eufalasio <guinaseuf@gmail.com>
2025-05-13 12:09:29 +0100

    added dynamic coloring

--- frontend/src/SlidingCover.jsx
+++ frontend/src/SlidingCover.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-
-
+import "./SlidingCover.css"
 const SlidingCover = () => {
     const [slider, setSlider] = React.useState(true);
 
     const handleClick = () => {
         setSlider(!slider);
-
+        document.documentElement.style.setProperty('--main-color', slider ? '#ffdddd' : '#b8f6fa');
     }
     return (
-        <div className={`sliding-cover ${slider ? `` : `sliding-cover-active`}`} onClick={handleClick}>
-
+        <div className={`sliding-cover ${slider ? `` : `sliding-cover-active`}`}>
+            <p onClick={handleClick}>Efidu</p>
+            
         </div>
 )
 }
